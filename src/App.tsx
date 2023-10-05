33f05c70b3cb956c244610ca96124ae96ab55077
benjuwan <benjuwan.banquet12@gmail.com>
2023-10-05 16:07:43 +0900

    脆弱性の更新（audit fix）と不要なコメントアウトの削除.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,3 @@
-/**
- * https://www.npmjs.com/package/@types/styled-components
- * npm install --save-dev styled-components @types/styled-components から
- * npm install --save @types/styled-components へ変更
- * 
- * 実際に機能したのはこっち。styled-components@latest で　最新版をインストールする記述内容
- * npm install --save-dev styled-components@latest @types/styled-components
-*/
-
 import { GlobalWrapper } from "./components/GlobalWrapper";
 
 export const App = () => {
